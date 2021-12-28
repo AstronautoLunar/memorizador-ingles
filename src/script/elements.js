65025e540e4579c14661988f864b5a708c6cfdf8
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-12-28 17:05:33 -0300

    :sparkles: Fez inputs do painel de start
    - Criado e estilizados os inputs
    
    - Criado um tratamento de digitos na entrada de números do input para
      caso o usuário digite uma letra ao invês do número
    
    - Feito animação dos inputs

--- src/script/elements.js
+++ src/script/elements.js
@@ -1,1 +1,4 @@
 const panelChoose = window.document.getElementById("panel-choose");
+const quantityAsk = window.document.getElementById("quantity-ask");
+const messageError = window.document.getElementById("error");
+const buttonStart = window.document.getElementById("button-start");
---
+++ src/script/verifyInputs.js
@@ -0,0 +1,63 @@
+const lettersAlphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "k",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+]
+
+let error = false;
+
+function applyStyleForModeError(error) {
+    if(error) {
+        messageError.style.height = "30px";
+        buttonStart.setAttribute("disabled", "");
+    } else {
+        messageError.style.height = "0";
+        buttonStart.removeAttribute("disabled");
+    }
+}
+
+quantityAsk.addEventListener("input", ({ target }) => {
+    const valueQuantityInput = target.value;
+    const lettersInput = valueQuantityInput.split("");
+
+    for(let letterInput of lettersInput) {
+        for(let letterAlphabet of lettersAlphabet) {
+            if(letterInput === letterAlphabet) {
+                error = true;
+
+                applyStyleForModeError(error);
+                break;
+            } else {
+                error = false;
+
+                applyStyleForModeError(error);
+            }
+        }
+    }
+})
+
+
+
