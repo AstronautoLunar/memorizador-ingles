afae5ef1a57179b3a13c95a835ee11aaa0994907
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2022-01-01 16:58:34 -0300

    Separado os arquivos do front-end em sua devida pasta

--- serve.js
+++ serve.js
@@ -4,6 +4,6 @@ const path = require("path");
 const app = express();
 const PORT = 4444;
 
-app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "src", "front-end")));
 
 app.listen(PORT, () => console.log(`localhost:${PORT}`));
