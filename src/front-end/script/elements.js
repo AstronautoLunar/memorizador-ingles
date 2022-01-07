c3028bdb71b9b15272b5c10a6a435f71ba39284e
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2022-01-07 00:14:33 -0300

    :sparkles: Fez navegação entre os paineis na interface no front-end

--- src/front-end/script/elements.js
+++ src/front-end/script/elements.js
@@ -1,8 +1,20 @@
+// Word Choice Panel Elements
+// Elementos do painel de escolha de palavras
 const panelChoose = window.document.getElementById("panel-choose");
 const quantityAsk = window.document.getElementById("quantity-ask");
 const messageError = window.document.getElementById("error");
 const buttonStart = window.document.getElementById("button-start");
 const panelChooseItems = window.document.getElementById("panel-choose-items");
+
+// Elements for functioning of panel navigation
+// Elementos para funcionamento de navegação de  paineis
+const listItemsNav = window.document.getElementById("list-items-nav");
+const panelAddWord = window.document.getElementById("panel-add-word");
+const panelModifyWord = window.document.getElementById("panel-modify-word");
+const panelDeleteWord = window.document.getElementById("panel-delete-word");
+
+// Word Quiz Panel Elements
+// Elementos do painel de quiz de palavras
 const panelQuiz = window.document.getElementById("panel-quiz");
 const wordSelected = window.document.getElementById("word-selected");
 const verifyButton = window.document.getElementById("verify");
@@ -11,4 +23,3 @@ const inputAnswer = window.document.getElementById("answer");
 const wordResponse = window.document.getElementById("word-response");
 const showWordButton = window.document.getElementById("show-word");
 const buttonOut = window.document.getElementById("button-out");
-const listItemsNav = window.document.getElementById("list-items-nav");
