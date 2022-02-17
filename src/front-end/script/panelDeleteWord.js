96351a147d3e4f4aa64abfa02dd3c58a26f64c54
Pyedrown <Pedrinho_fsf@hotmail.com>
2022-02-17 00:54:54 -0300

    Adicionado lógica no front-end que permite apagar dados salvos no json através da api

--- src/front-end/script/panelDeleteWord.js
+++ src/front-end/script/panelDeleteWord.js
@@ -31,61 +31,61 @@ function verifyConfirmPanelDelete() {
     }
 }
 
-async function generateItemsOfDeleteWord() {
-    async function renderItems() {
-        const response = await fetch("http://localhost:4444/api/getWords");
-        const dataJson = await response.json();
-        const data = JSON.parse(dataJson);
+async function renderItems() {
+    const response = await fetch("http://localhost:4444/api/getWords");
+    const dataJson = await response.json();
+    const data = JSON.parse(dataJson);
 
-        areaWordsDelete.innerHTML = "";
+    areaWordsDelete.innerHTML = "";
 
-        data.words.forEach(({
-            id,
-            US,
-            BR,
-            date
-        }) => {
-            const { original } = randomColorRgb();
-    
-            return (
-                areaWordsDelete.innerHTML += `
-                    <div 
-                        class="area-item-delete-word"
-                        style="
-                            color: ${original};
-                            border-color: ${original};
-                        "
-                    >
-                        <div class="area-item-text">
-                            <span 
-                                class="item item-delete-word"
-                                data-translate="${BR}"
-                                data-original="${US}"
-                                data-id="${id}"
-                                data-datetime="${date}"
-                                data-color="${original}"
-                                data-selected="false"
-                            >
-                                ${ US }
-                            </span>
-                        </div>
-                        <button 
-                            class="button-delete-word"
+    data.words.forEach(({
+        id,
+        US,
+        BR,
+        date
+    }) => {
+        const { original } = randomColorRgb();
+
+        return (
+            areaWordsDelete.innerHTML += `
+                <div 
+                    class="area-item-delete-word"
+                    style="
+                        color: ${original};
+                        border-color: ${original};
+                    "
+                >
+                    <div class="area-item-text">
+                        <span 
+                            class="item item-delete-word"
+                            data-translate="${BR}"
+                            data-original="${US}"
                             data-id="${id}"
+                            data-datetime="${date}"
+                            data-color="${original}"
                             data-selected="false"
                         >
-                            <img
-                                class="icon-delete-word"
-                                src="assets/icons/icon-delete-black.svg"
-                                alt="icon delete word"
-                            />
-                        </button>
+                            ${ US }
+                        </span>
                     </div>
-                `
-            )
-        });
-    }
+                    <button 
+                        class="button-delete-word"
+                        data-id="${id}"
+                        data-selected="false"
+                    >
+                        <img
+                            class="icon-delete-word"
+                            src="assets/icons/icon-delete-black.svg"
+                            alt="icon delete word"
+                        />
+                    </button>
+                </div>
+            `
+        )
+    });
+}
 
+async function generateItemsOfDeleteWord() {
     await renderItems();
 
     const buttonsDeleteItems = window.document.querySelectorAll(".button-delete-word");
@@ -115,13 +115,6 @@ async function generateItemsOfDeleteWord() {
                 itemsSelectedForDelete = newListItems;
             }
 
-            // if(itemsSelectedForDelete.length) {
-            //     confirmDeleteWords.style.height = "110px";
-            //     confirmDeleteWords.style.padding = "24px 0";
-            // } else {
-            //     confirmDeleteWords.style.height = "";
-            //     confirmDeleteWords.style.padding = "";
-            // }
             verifyConfirmPanelDelete();
         })
     }
@@ -129,22 +122,29 @@ async function generateItemsOfDeleteWord() {
 
 generateItemsOfDeleteWord();
 
-buttonDelete.addEventListener("click", () => {
-    /**
-     * const headers = new Headers({
-                    "Content-Type": "Application/json"
-                });
-    
-                await fetch("http://localhost:4444/api/deleteWord", {
-                    headers,
-                    method: "DELETE",
-                    body: JSON.stringify({ id })
-                });
-    
-                setTimeout(async () => {
-                    await renderItems();
-                }, 2000);
-     */
+buttonDelete.addEventListener("click", async () => {
+    const headers = new Headers({
+        "Content-Type": "Application/json"
+    });
+
+    const responseFetch = await fetch("http://localhost:4444/api/deleteWord", {
+        headers,
+        method: "DELETE",
+        body: JSON.stringify({
+            listId: itemsSelectedForDelete
+        })
+    });
+
+    if(responseFetch.status === 200) {
+        setTimeout(async () => {
+            await renderItems();
+        }, 3000);
+    }
+
+    itemsSelectedForDelete = [];
+    verifyConfirmPanelDelete();
+
+    alert(`${responseFetch.status} ${await responseFetch.text()}`)
 });
 
 buttonDeselect.addEventListener("click", () => {
